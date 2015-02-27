0e9a2db310c59d6038bd2e58e3464b7c61fa511d
Victor Lowther <victor.lowther@gmail.com>
2015-02-27 16:13:42 -0600

    Clean up formatting in namespaces.go

--- namespaces.go
+++ namespaces.go
@@ -1,12 +1,14 @@
 package wsman
 
-const NS_WSMAN = "http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd"
-const NS_WSMID = "http://schemas.dmtf.org/wbem/wsman/identity/1/wsmanidentity.xsd"
-const NS_WSDL = "http://schemas.xmlsoap.org/wsdl"
-const NS_WSA = "http://schemas.xmlsoap.org/ws/2004/08/addressing"
-const NS_WSA10 = "http://www.w3.org/2005/08/addressing"
-const NS_WSAM = "http://www.w3.org/2007/05/addressing/metadata"
-const NS_WSME = "http://schemas.xmlsoap.org/ws/2004/08/eventing"
-const NS_WSMEN = "http://schemas.xmlsoap.org/ws/2004/09/enumeration"
-const NS_WSMT = "http://schemas.xmlsoap.org/ws/2004/09/transfer"
-const NS_WSP = "http://schemas.xmlsoap.org/ws/2004/09/policy"
+const (
+	NS_WSMAN = "http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd"
+	NS_WSMID = "http://schemas.dmtf.org/wbem/wsman/identity/1/wsmanidentity.xsd"
+	NS_WSDL  = "http://schemas.xmlsoap.org/wsdl"
+	NS_WSA   = "http://schemas.xmlsoap.org/ws/2004/08/addressing"
+	NS_WSA10 = "http://www.w3.org/2005/08/addressing"
+	NS_WSAM  = "http://www.w3.org/2007/05/addressing/metadata"
+	NS_WSME  = "http://schemas.xmlsoap.org/ws/2004/08/eventing"
+	NS_WSMEN = "http://schemas.xmlsoap.org/ws/2004/09/enumeration"
+	NS_WSMT  = "http://schemas.xmlsoap.org/ws/2004/09/transfer"
+	NS_WSP   = "http://schemas.xmlsoap.org/ws/2004/09/policy"
+)
